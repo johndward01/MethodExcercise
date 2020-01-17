a65915958f723dc84cd5c201f95d9372c89cf49e
John Ward <johndward01@yahoo.com>
2020-01-17 13:52:43 -0600

    added functions and 1 method overload

--- MethodExcercise_1/MethodExcercise_1/Program.cs
+++ MethodExcercise_1/MethodExcercise_1/Program.cs
@@ -10,6 +10,12 @@ namespace MethodExcercise_1
             return result;
         }
 
+        public static decimal Sum(decimal x, decimal y)
+        {
+            decimal sum = x + y;
+            return sum;
+        }
+
         public static int Subtract(int x, int y)
         {
             int result = x + y;
@@ -27,21 +33,40 @@ namespace MethodExcercise_1
             int result = x / y;
             return result;
         }
+
+        public static string Add(int x, int y, bool z)
+        {
+            if (z == true)
+            {
+                int result = x + y;
+                if (result == 1)
+                    return $"{result} dollar";
+                else
+                    return $"{result} dollars";
+            }
+            else
+                return "";
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(Add(3, 4, true));
+
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
+
             Console.WriteLine("What is your favorite color?");
             string color = Console.ReadLine();
+
             Console.WriteLine("What is your favorite animal?");
             string animal = Console.ReadLine();
+
             Console.WriteLine("What is your favorite band?");
             string band = Console.ReadLine();
 
             Console.WriteLine($"Hi! My name is {name} and my favorite " +
-                $"color is {color}.\n" +
-                $"My favorite animal is a {animal} and my " +
-                $"favorite band is {band}.");
+                            $"color is {color}.\n" +
+                            $"My favorite animal is a {animal} and my " +
+                            $"favorite band is {band}.");
 
         }
     }
