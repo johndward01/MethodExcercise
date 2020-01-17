a578395bf72dee90e2a0a6f387efe40559614eed
John Ward <johndward01@yahoo.com>
2020-01-17 10:37:16 -0600

    excercise finished

---
+++ MethodExcercise_1/MethodExcercise_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodExcercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string band = Console.ReadLine();
+
+            Console.WriteLine($"Hi! My name is {name} and my favorite " +
+                $"color is {color}.\n" +
+                $"My favorite animal is a {animal} and my " +
+                $"favorite band is {band}.");
+        }
+    }
+}
